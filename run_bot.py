05514dc5e8b6313bf7b09fcb58c6548e9a130125
JenerikMind <darkheart_wolf@yahoo.com>
2018-03-04 15:49:21 -0500

    added startup prompt

--- run_bot.py
+++ run_bot.py
@@ -1,5 +1,19 @@
 import bot
 import keyboard
 
+setup = True
 bot.test_import()
-bot.load_coords()
+
+while setup:
+	user_input = input("""
+	
+		1) Setup Positions
+		2) Load Positions
+	
+	""")
+	if user_input == '1':
+		bot.key_setup()
+		setup = False
+	else:
+		bot.load_coords()
+		setup = False
